23e325b08775dab968df327abbb8e10201dda854
Naxisigut <834525217@qq.com>
2024-10-02 23:34:07 +0800

    feat: 删除utils

--- utils/array.ts
+++
@@ -1,5 +0,0 @@
-/* 获取数组内的随机元素 */
-export function getRandomElement(array: Array<any>) {
-  let randomIndex = Math.floor(Math.random() * array.length);
-  return array[randomIndex];
-}
--- utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './array'
-export * from './file'
-export * from './number'
-export * from './other'
